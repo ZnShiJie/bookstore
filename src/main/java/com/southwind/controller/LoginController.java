00e6c9586a396d513dbc5e20bfb0a5a509fb6ab7
ZnWorld <ZnWorld@qq.com>
2023-01-16 22:12:41 +0800

    hot fix first commit

--- src/main/java/com/southwind/controller/LoginController.java
+++ src/main/java/com/southwind/controller/LoginController.java
@@ -33,7 +33,7 @@ public class LoginController {
         switch (type) {
             case "reader":
                 Reader reader = readerService.getReader(password, username);
-                //System.out.println(reader + "\n\n\n\n\n\n\n");
+                System.out.println(reader + "\n\n\n\n\n\n\n");
                 if (reader != null) {
                     request.getSession().setAttribute("reader", reader);
                     return "redirect:/reader/allBookReader";
